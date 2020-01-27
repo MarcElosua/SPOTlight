7034d0db2d9f94c94e7074fe3e5d01aeb1fe56fa
MarcElosua <elosua.marc@gmail.com>
2020-01-27 16:11:34 +0100

    added top_n_predictions_fun.R

---
+++ R/top_n_predictions_fun.R
@@ -0,0 +1,21 @@
+#' This function gets a distance matrix and returns a list with the indices of the lowest (best) prediction indices
+#'
+#' @param dist_mtrx Object of class matrix. Distance matrix to evaluate
+#' @param n number of best predictions to return
+#' @return list of nrows(dist_mtrx) elements with ~n elements each
+#' @export
+#' @examples
+#'
+
+top_n_predictions <- function(dist_mtrx,n){
+
+  if( ! is.matrix(dist_mtrx) ) {stop("ERROR: dist_mtrx must be a matrix object!")}
+
+  min_error <- Rfast::rownth(x = dist_mtrx, elems = rep(n, nrow(dist_mtrx)))
+
+  # Get indices over which to calculate JD
+  JD_indices <- lapply(1:length(min_error), function(i){
+    which(dist_mtrx[i,] <= min_error[i])
+  })
+  return(JD_indices)
+}
