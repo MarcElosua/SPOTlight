77f36d40cb28c3d4e7f72aff0ec1c771c93367b4
Marc Elosua <elosua.marc@gmail.com>
2020-01-30 11:40:28 +0100

    lint downsample_se_obj

--- R/downsample_se_obj_fun.R
+++ R/downsample_se_obj_fun.R
@@ -7,43 +7,40 @@
 #' @export
 #' @examples
 
-downsample_se_obj <- function(se_obj, clust_vr, cluster_markers_all){
+downsample_se_obj <- function(se_obj, clust_vr, cluster_markers_all) {
 
   # Check variables
-  if(is(se_obj)!="Seurat") {stop("ERROR: se_obj must be a Seurat object!")}
-  if(!is.character(clust_vr)){stop("ERROR: clust_vr must be a character string!")}
-  if(!is.data.frame(cluster_markers_all)){stop("ERROR: cluster_markers_all must be a data frame object returned from Seurat::FindAllMarkers()!")}
+  if (is(se_obj) != "Seurat") stop("ERROR: se_obj must be a Seurat object!")
+  if (!is.character(clust_vr)) stop("ERROR: clust_vr must be a character string!")
+  if (!is.data.frame(cluster_markers_all)) stop("ERROR: cluster_markers_all must be a data frame object returned from Seurat::FindAllMarkers()!")
 
-  #load required packages
-  # cat("Loading packages...", sep="\n")
+  # load required packages
   suppressMessages(require(Seurat))
   suppressMessages(require(purrr))
   suppressMessages(require(dplyr))
   suppressMessages(require(tibble))
 
-  se_obj$seurat_clusters <- droplevels(factor(se_obj@meta.data[,clust_vr]))
+  se_obj$seurat_clusters <- droplevels(factor(se_obj@meta.data[, clust_vr]))
 
-  if(length(VariableFeatures(se_obj)) == 0) se_obj <- Seurat::FindVariableFeatures(object = se_obj, nfeatures = 5000)
+  if (length(Seurat::VariableFeatures(se_obj)) == 0) se_obj <- Seurat::FindVariableFeatures(object = se_obj, nfeatures = 5000)
 
   #### Combine marker genes and highest variable genes and subset genes ####
   keep_genes <- unique(c(VariableFeatures(se_obj), cluster_markers_all$gene))
 
   #### Get cell IDs to subset by cluster ####
-  keep_ids <- lapply(split(se_obj@meta.data, se_obj@meta.data$seurat_clusters), function(subdf){
+  keep_ids <- lapply(split(se_obj@meta.data, se_obj@meta.data$seurat_clusters), function(subdf) {
     # Determine n_sample, if the size of the group is < 100 use all the group, if not just use 100
     n_sample <- if_else(nrow(subdf) < 100, nrow(subdf), 100L)
     # Subset a random selection of that group and get the identifiers
-    tmp_ds <- subdf[sample(1:nrow(subdf), n_sample),] %>%
-      tibble::rownames_to_column('ID') %>%
+    tmp_ds <- subdf[sample(seq_len(nrow(subdf)), n_sample), ] %>%
+      tibble::rownames_to_column("ID") %>%
       dplyr::pull(ID)
     return(tmp_ds)
   }) %>%
     purrr::flatten_chr() # flatten the list into a vector
 
   #### Subset seurat object ####
-  se_obj <- se_obj[keep_genes,keep_ids]
-  # saveRDS(se_obj, file = sprintf('%s/%s/seurat_umap_%s.RDS', an_00, robj_dir, id_comp))
+  se_obj <- se_obj[keep_genes, keep_ids]
 
   return(se_obj)
 }
-
