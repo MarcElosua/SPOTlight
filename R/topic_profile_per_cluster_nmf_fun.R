2f5fdacb5d3303852499bee9a8f85b040bc98e00
MarcElosua <elosua.marc@gmail.com>
2020-10-14 09:54:49 +0200

    removed clust vr from R/topic_profile_per_cluster_nmf_fun.R

--- R/topic_profile_per_cluster_nmf_fun.R
+++ R/topic_profile_per_cluster_nmf_fun.R
@@ -2,32 +2,29 @@
 #'
 #' @param h Object of class matrix, coefficient matrix from NMF model.
 #' @param train_cell_clust Object of class vector with cluster of the cells used to train the model.
-#' @param clust_vr Object of class character. Name of the variable containing the cell clustering.
 #' @return This function returns a list where the first element is a matrix with the topic profiles of all possible combinations and the 2nd element is the cell composition of each spot.
 #' @export
 #' @examples
 #'
 
 topic_profile_per_cluster_nmf <- function(h,
-                                          train_cell_clust,
-                                          clust_vr) {
+                                          train_cell_clust) {
 
   # Check variables
   if (!is(h, "matrix")) stop("ERROR: h must be a matrix object!")
   if (! is(train_cell_clust, "vector")) stop("ERROR: train_cell_clust must be a vector/list object!")
-  if (!is.character(clust_vr)) stop("ERROR: clust_vr must be a character string!")
 
   # Loading libraries
   suppressMessages(require(tibble))
   suppressMessages(require(dplyr))
 
   h_ds <- data.frame(t(h))
-  h_ds[, clust_vr] <- train_cell_clust
+  h_ds[, "clust_vr"] <- train_cell_clust
 
   ct_topic_profiles <- h_ds %>%
-    dplyr::group_by(!!! syms(clust_vr)) %>%
+    dplyr::group_by(clust_vr) %>%
     dplyr::summarise_all(list(median)) %>%
-    tibble::column_to_rownames(clust_vr) %>%
+    tibble::column_to_rownames("clust_vr") %>%
     as.matrix()
 
   ct_topic_profiles_t <- t(ct_topic_profiles)
