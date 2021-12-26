1be6fa3cb82621be26845e39bf210d655d352891
MarcElosua <elosua.marc@gmail.com>
2021-12-26 18:55:10 +0100

    topicProfiles tests

---
+++ tests/testthat/test-topicProfiles.R
@@ -0,0 +1,27 @@
+set.seed(123)
+x <- .mock_sc(nc = 50, nt = 3)
+y <- .mock_sp(x)
+z <- .get_mgs(x)
+res <- SPOTlight(x, y,
+    groups = x$type,
+    mgs = z,
+    group_id = "type",
+    verbose = FALSE)
+
+test_that("plotTopicProfiles common", {
+    p <- plotTopicProfiles(res[[3]], x$type, facet = FALSE)
+    expect_is(p, "ggplot")
+    expect_equal(nrow(p$data), 9)
+    expect_equal(ncol(p$data), 3)
+    g <- ggplot_build(p)
+    expect_equal(unique(g$data[[1]]$colour), c("#0000FF", "#D3D3D3"))
+})
+
+test_that("plotTopicProfiles facet", {
+    p <- plotTopicProfiles(res[[3]], x$type, facet = TRUE)
+    expect_is(p, "ggplot")
+    expect_equal(nrow(p$data), 150)
+    expect_equal(ncol(p$data), 4)
+    g <- ggplot_build(p)
+    expect_equal(unique(g$data[[1]]$colour), c("#0000FF", "#D3D3D3"))
+})
